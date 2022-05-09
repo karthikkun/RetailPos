24b532913046a9edc262a285bcd116df110ab0b1
karthik reddy <karthik.kun009@gmail.com>
2022-05-09 12:52:07 +0530

    project setup

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import 'antd/dist/antd.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Homepage from './pages/Homepage';
+import Items from './pages/Items';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+            <Route path="/home" element={<Homepage />}/>
+            <Route path="/items" element={<Items />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
