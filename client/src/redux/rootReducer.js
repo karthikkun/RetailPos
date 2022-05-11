d282ff70fe1554984834b8ad448dd0b2244d98bf
karthik reddy <karthik.kun009@gmail.com>
2022-05-11 15:19:40 +0530

    list items | add items

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -14,21 +14,13 @@ export const rootReducer = (state = initialState, action) => {
         case 'UPDATE_CART' : return {
             ...state,
             cartItems : state.cartItems.map((item) => 
-<<<<<<< Updated upstream
-                item._id == action.payload._id 
-=======
                 item._id === action.payload._id 
->>>>>>> Stashed changes
                 ? {...item, quantity : action.payload.quantity} : item
             )
         }
         case 'DELETE_FROM_CART' : return {
             ...state,
-<<<<<<< Updated upstream
-            cartItems : state.cartItems.filter((item) => item._id != action.payload._id)
-=======
             cartItems : state.cartItems.filter((item) => item._id !== action.payload._id)
->>>>>>> Stashed changes
         }
         case 'SHOW_LOADING' : return {
             ...state,
